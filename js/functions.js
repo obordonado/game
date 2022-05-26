7e33ba59a74caf7ceb63e8353b6ac67238f1e6d0
Óscar Bordonado <obordonado@protonmail.com>
2022-05-26 20:57:31 +0200

    página 2 prueba

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,14 @@
-// const screenSwitch = (pantallaDestino) => {
+const screenSwitch = (pantallaDestino) => {
 
-//     let destino = document.getElementById(pantallaDestino);
-//     destino.style.display = "flex";
+    let destino = document.getElementById(pantallaDestino);
+    destino.style.display = "flex";
 
-//     let arrayPantallas = ["pantalla1","pantalla2","pantalla3","pantalla4","pantalla5"];
+    let arrayPantallas = ["pantalla1","pantalla2","pantalla3","pantalla4","pantalla5"];
 
-//     for(let pantalla of arrayPantallas){
-//         if(pantalla != pantallaDestino){
-//             document.getElementById(pantalla).style.display = "none";
-//         };
-//     };
+    for(let pantalla of arrayPantallas){
+        if(pantalla != pantallaDestino){
+            document.getElementById(pantalla).style.display = "none";
+        };
+    };
    
-// };
+};
