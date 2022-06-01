7fde303d28788c2d914879c7e8bf619ac5ed2250
Óscar Bordonado <obordonado@protonmail.com>
2022-06-01 08:35:15 +0200

    eliminado innecesario

--- js/main.js
+++ js/main.js
@@ -8,19 +8,23 @@ let bichoIzquierda = document.getElementById("elegidoUnoData");
 let bichoDerecha = document.getElementById("elegidoDosData");
 let imagenIzquierda = document.getElementById("elegidoUno");
 let imagenDerecha = document.getElementById("elegidoDos");
-
-
+let elegidoIzq = document.getElementsByClassName("elegido-izq");
+let elegidoDer = document.getElementsByClassName("elegidoDer");
 
 
 const screenSwitch = (pantallaDestino) => {
   let destino = document.getElementById(pantallaDestino);
-  destino.style.display = "flex";
-  let pantallas = ["pantalla1","pantalla2","pantalla3","pantalla4","pantalla5"];
-  for(let pantalla of pantallas){
-    if(pantalla != pantallaDestino)
-    {document.getElementById(pantalla).style.display = "none";
+
+  setTimeout(()=>{
+    destino.style.display = "flex";
+    let pantallas = ["pantalla1","pantalla2","pantalla3","pantalla4","pantalla5"];
+    for(let pantalla of pantallas){
+      if(pantalla != pantallaDestino)
+      {document.getElementById(pantalla).style.display = "none";
+    }
   }
-}
+  },300);
+
 }
 
 const selecciona = (bicho) => {
@@ -30,64 +34,39 @@ const selecciona = (bicho) => {
     imagenDerecha.innerHTML = `PLAYER 2 <br><br>Nombre : ${jugador[1].nombre} <br> Vida : ${jugador[1].puntosvida} <br> Fuerza : ${jugador[1].fuerza} <br> Defensa : ${jugador[1].defensa}`;
   }
 }
+// elegidoIzq.innerHTML = `PLAYER 1 <br><br>Nombre : ${jugador[0].nombre} <br> Vida : ${jugador[0].puntosvida} <br> Fuerza : ${jugador[0].fuerza} <br> Defensa : ${jugador[0].defensa}`;
+
+
+
+
 
 // OJO A RESOLUCION PANTALLA PORTATIL
 
-        //Una vez hemos introducido el coche en el array...
-        //comprobamos si la longitud del array es 2. 
-        //Eso significaría que ya tenemos los dos coches y que hay que cambiar de pantalla
-
-//         if(jugadores.length === 2){
-
-//             setTimeout(()=>{
-
-//                 //Diseñamos el contenido de la pantalla3 antes de mostrarla dando el cambiazo
-//                 vistaVersus.innerHTML = `<div class='hijoVersus'><img class='fotoVersus' src='img/${jugadores[0].modelo}.jpg' alt='asdfasdf'></div><div class='hijo3'><img class='fotoVersus' src='img/logovs.jpg'/><div class='hijoVersus'><img class='fotoVersus' src='img/${jugadores[1].modelo}.jpg' alt='asdfasdf'></div>`;
-//                 screenSwitch("pantalla3");
-
-//                 setTimeout(()=>{
-
-//                     cocheIzquierda.innerHTML = `<div><img class='cocheRun' src='img/${jugadores[0].modelo}.jpg'/></div><div>${jugadores[0].marca}</div><div id='metrosIzquierda'>${jugadores[0].metros}</div>`;
-//                     cocheDerecha.innerHTML = `<div><img class='cocheRun' src='img/${jugadores[1].modelo}.jpg'/></div><div>${jugadores[1].marca}</div><div id='metrosDerecha'>${jugadores[1].metros}</div>`;
-
-//                     metrosIzquierda = document.getElementById("metrosIzquierda");
-//                     metrosDerecha = document.getElementById("metrosDerecha");
-
-//                     screenSwitch("pantalla4");
-
-                    
-//                 },2000);
-//             },500);
-//         }
-
-//     }               
-
-// };
 
 // const play = () => {
-    
-//     let random = Math.round(Math.random());
 
-//     jugadores[random].acelerar();
+// let random = Math.round(Math.random());
 
-//     if(jugadores[random].metros >= 200){
+// jugadores[random].acelerar();
+
+// if(jugadores[random].metros >= 200){
 
 
 
-//         setTimeout(()=>{
+//   setTimeout(()=>{
 
-//             winnerIs.innerHTML = `${jugadores[random].marca}`;
+//       winnerIs.innerHTML = `${jugadores[random].marca}`;
 
-//             screenSwitch("pantalla5");
-//         },500)
-//     }
+//       screenSwitch("pantalla5");
+//   },500)
+// }
 
-//     if(random == 0){
+// if(random == 0){
 
-//         metrosIzquierda.innerHTML = `${jugadores[random].metros}`;
-//     } else {
-//         metrosDerecha.innerHTML = `${jugadores[random].metros}`;
-//     }
+//   metrosIzquierda.innerHTML = `${jugadores[random].metros}`;
+// } else {
+//   metrosDerecha.innerHTML = `${jugadores[random].metros}`;
+// }
 
 
 // };
