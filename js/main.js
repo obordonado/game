cf645f471c6641d6ce699fd3d6574a239eac3e05
Óscar Bordonado <obordonado@protonmail.com>
2022-06-01 20:22:03 +0200

    sin resolver problema pantalla 2 undefined reading nombre y sin acceso a los datos para pantalla 3

--- js/main.js
+++ js/main.js
@@ -3,83 +3,50 @@ let jugador = [];
 
 // let ganador = "";
 
-
-let bichoIzquierda = document.getElementById("elegidoUnoData");
-let bichoDerecha = document.getElementById("elegidoDosData");
+//Apunta a los stats de los jugadores en ventanas inferiores de pantalla 2
 let imagenIzquierda = document.getElementById("elegidoUno");
-let imagenDerecha = document.getElementById("elegidoDos");
-let elegidoIzq = document.getElementsByClassName("elegido-izq");
-let elegidoDer = document.getElementsByClassName("elegidoDer");
+let imagenDerecha =   document.getElementById("elegidoDos");
+
+
+//Apunta a los stats de los jugadores en ventanas superiores de pantalla 3
+let elegidoIzq =      document.getElementsByClassName("elegidoIzq");
+let elegidoDer =      document.getElementsByClassName("elegidoDer");
+
+//Apunta a las imágenes de jugadores pantalla 3
+let imagenPagTresIz = document.getElementsByClassName("imagenPagTresIzq");
+let imagenPagTresDe = document.getElementsByClassName("imagenPagTresDer");
 
 
 const screenSwitch = (pantallaDestino) => {
+
   let destino = document.getElementById(pantallaDestino);
-
-  setTimeout(()=>{
-    destino.style.display = "flex";
-    let pantallas = ["pantalla1","pantalla2","pantalla3","pantalla4","pantalla5"];
-    for(let pantalla of pantallas){
-      if(pantalla != pantallaDestino)
-      {document.getElementById(pantalla).style.display = "none";
-    }
-  }
-  },300);
-
-}
+  destino.style.display = "flex";
+  let pantallas = ["pantalla1","pantalla2","pantalla3"];
+  for(let pantalla of pantallas){
+    if(pantalla != pantallaDestino){
+      document.getElementById(pantalla).style.display = "none";
+    };
+  };
+};
 
 const selecciona = (bicho) => {
   if(jugador.length < 2){
     jugador.push(allPlayers[bicho]);
     imagenIzquierda.innerHTML = `PLAYER 1 <br><br>Nombre : ${jugador[0].nombre} <br> Vida : ${jugador[0].puntosvida} <br> Fuerza : ${jugador[0].fuerza} <br> Defensa : ${jugador[0].defensa}`;
-    imagenDerecha.innerHTML = `PLAYER 2 <br><br>Nombre : ${jugador[1].nombre} <br> Vida : ${jugador[1].puntosvida} <br> Fuerza : ${jugador[1].fuerza} <br> Defensa : ${jugador[1].defensa}`;
-  }
-}
-// elegidoIzq.innerHTML = `PLAYER 1 <br><br>Nombre : ${jugador[0].nombre} <br> Vida : ${jugador[0].puntosvida} <br> Fuerza : ${jugador[0].fuerza} <br> Defensa : ${jugador[0].defensa}`;
+    imagenDerecha.innerHTML =   `PLAYER 2 <br><br>Nombre : ${jugador[1].nombre} <br> Vida : ${jugador[1].puntosvida} <br> Fuerza : ${jugador[1].fuerza} <br> Defensa : ${jugador[1].defensa}`;
 
-
-
-
-
-// OJO A RESOLUCION PANTALLA PORTATIL
-
-
-// const play = () => {
-
-// let random = Math.round(Math.random());
-
-// jugadores[random].acelerar();
-
-// if(jugadores[random].metros >= 200){
-
-
-
-//   setTimeout(()=>{
-
-//       winnerIs.innerHTML = `${jugadores[random].marca}`;
-
-//       screenSwitch("pantalla5");
-//   },500)
-// }
-
-// if(random == 0){
-
-//   metrosIzquierda.innerHTML = `${jugadores[random].metros}`;
-// } else {
-//   metrosDerecha.innerHTML = `${jugadores[random].metros}`;
-// }
-
-
-// };
+  };
+};
 
 // const limpiar = () => {
 
-//     jugadores = [];
+//     jugador = [];
 
 //     ganador = "";
 
-//     metrosIzquierda.innerHTML = ``;
+//     danoIzquierda.innerHTML = ``;
 
-//     metrosDerecha.innerHTML = ``;
+//     danosDerecha.innerHTML = ``;
 
 //     coche1.metros = 0;
 //     coche2.metros = 0;
@@ -89,6 +56,67 @@ const selecciona = (bicho) => {
 //     setTimeout(()=>{
 
 //         screenSwitch("pantalla1");
-//     },500);
+//     },2000);
 
+// };
+
+// elegidoIzq.innerHTML = `PLAYER 1 <br><br>Nombre : ${jugador[0].nombre} <br> Vida : ${jugador[0].puntosvida} <br> Fuerza : ${jugador[0].fuerza} <br> Defensa : ${jugador[0].defensa}`;
+// elegidoDer.innerHTML = `PLAYER 1 <br><br>Nombre : ${jugador[0].nombre} <br> Vida : ${jugador[0].puntosvida} <br> Fuerza : ${jugador[0].fuerza} <br> Defensa : ${jugador[0].defensa}`;
+
+// if(jugador.length===2){
+//   setTimeout(()=>{
+
+//     batalla.innerHTML = `
+
+//     <div class="cont-lados">
+//       <div class="cont-barra">
+//         <div class="barra-vida"></div>
+//       </div>
+//       <div class="cont-stats">
+//         <div class="elegidoIzq elegido3"></div>
+//       </div>
+//       <div class="cont-imagen">
+//         <div class="imagenPagTresIzq">    
+//           <img src='img/${jugador[0]}.png'>   
+//         </div>
+//       </div>
+//       <div class="cont-boton">
+//         <div class="boton">ATTACK !!</div>
+//         <div class="boton">HEAL</div>
+//       </div>
+//     </div>
+        
+//     <div class="cont-lados">
+//       <div class="cont-barra">
+//         <div class="barra-vida"></div>
+//       </div>
+//       <div class="cont-stats">
+//         <div class="elegidoDer elegido3"></div> 
+//       </div>
+//       <div class="cont-imagen">
+//         <div class="imagenPagTresDer">        
+//         <img src='img/${jugador[1]}.png'>       
+//         </div>
+//       </div>
+//       <div class="cont-boton">
+//         <div class="boton">ATTACK !!</div>
+//         <div class="boton">HEAL</div>
+//       </div>
+//     </div>    
+//     `;
+    
+//     // screenSwitch("pantalla3");
+
+
+//     setTimeout(()=>{
+
+//       elegidoIzq.innerHTML = "PLAYER 1 <br><br>Nombre : " + `${jugador[0].nombre}`+ " <br> +Vida : " + `${jugador[0].puntosvida}` + "<br> Fuerza : " + `${jugador[0].fuerza}` + "<br> Defensa : " + `${jugador[0].defensa}`;
+//       elegidoDer.innerHTML = "PLAYER 2 <br><br>Nombre : " + `${jugador[1].nombre}`+ " <br> +Vida : " + `${jugador[1].puntosvida}` + "<br> Fuerza : " + `${jugador[1].fuerza}` + "<br> Defensa : " + `${jugador[1].defensa}`;
+
+//       imagenPagTresIz.innerHTML =` <div class="imagenPagTresDer"> <img src='img/${jugador[0]}.png'> </div> `
+      
+//       imagenPagTresDe.innerHTML= ` <div class="imagenPagTresDer"> <img src='img/${jugador[1]}.png'> </div> `
+
+//     },200);
+//   },200);
 // };
